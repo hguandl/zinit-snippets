e7973f55b206efe0383b6ada355e8ef2fe5a320b
Hao Guan <10684225+hguandl@users.noreply.github.com>
2023-02-17 16:11:30 +1000

    Update homebrew-miniconda.zsh

--- mac_arm64/homebrew-miniconda.zsh
+++ mac_arm64/homebrew-miniconda.zsh
@@ -1,4 +1,3 @@
-
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 __conda_setup="$('/opt/homebrew/Caskroom/miniconda/base/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
