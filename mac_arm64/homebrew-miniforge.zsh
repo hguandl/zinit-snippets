6f30d7f3325eb09893189d0851eb22c100198c3c
Hao Guan <10684225+hguandl@users.noreply.github.com>
2024-12-17 20:59:21 +0800

    Update homebrew-miniforge.zsh

--- mac_arm64/homebrew-miniforge.zsh
+++ mac_arm64/homebrew-miniforge.zsh
@@ -12,3 +12,16 @@ else
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba shell init' !!
+export MAMBA_EXE='/opt/homebrew/Caskroom/miniforge/base/condabin/mamba';
+export MAMBA_ROOT_PREFIX='/opt/homebrew/Caskroom/miniforge/base';
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --root-prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    alias mamba="$MAMBA_EXE"  # Fallback on help from mamba activate
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
